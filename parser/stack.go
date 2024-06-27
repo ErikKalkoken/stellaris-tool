b312129c86c7f4740207a4cb1b6192f3a7336c91
ErikKalkoken <erik.kalkoken@gmail.com>
2024-06-27 23:41:28 +0200

    Add stack

---
+++ parser/stack.go
@@ -0,0 +1,47 @@
+package parser
+
+import (
+	"errors"
+	"slices"
+)
+
+var errStackEmpty = errors.New("stack is empty")
+
+// stack is a generic stack with limited size
+type stack[T any] struct {
+	l    []T
+	size int
+}
+
+// newStack returns a new stack
+func newStack[T any](size int) stack[T] {
+	if size < 1 {
+		panic("size must be 1 or larger")
+	}
+	s := stack[T]{
+		l:    make([]T, 0, size),
+		size: size,
+	}
+	return s
+}
+
+// push pushed a new element on the stack.
+// If the stack reached it's limit it will discard the oldest element.
+func (s *stack[T]) push(v T) {
+	if len(s.l) == s.size {
+		s.l = slices.Delete(s.l, 0, 1)
+	}
+	s.l = append(s.l, v)
+}
+
+// pop removes and returns the newest element from the stack.
+func (s *stack[T]) pop() (T, error) {
+	if len(s.l) == 0 {
+		var x T
+		return x, errStackEmpty
+	}
+	last := len(s.l) - 1
+	v := s.l[last]
+	s.l = slices.Delete(s.l, last, last+1)
+	return v, nil
+}
---
+++ parser/stack_test.go
@@ -0,0 +1,58 @@
+package parser
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStack(t *testing.T) {
+	t.Run("can push and pop 1 element", func(t *testing.T) {
+		s := newStack[int](5)
+		s.push(5)
+		x, err := s.pop()
+		if assert.NoError(t, err) {
+			assert.Equal(t, 5, x)
+		}
+	})
+	t.Run("can push and pop 2 elements", func(t *testing.T) {
+		s := newStack[int](5)
+		s.push(5)
+		s.push(3)
+		x, err := s.pop()
+		if assert.NoError(t, err) {
+			assert.Equal(t, 3, x)
+		}
+		x, err = s.pop()
+		if assert.NoError(t, err) {
+			assert.Equal(t, 5, x)
+		}
+	})
+	t.Run("should raise error when trying to pop from empty stack", func(t *testing.T) {
+		s := newStack[int](5)
+		_, err := s.pop()
+		assert.ErrorIs(t, err, errStackEmpty)
+	})
+	t.Run("should only store the last x elements", func(t *testing.T) {
+		s := newStack[int](3)
+		s.push(1)
+		s.push(2)
+		s.push(3)
+		s.push(4)
+		assert.Len(t, s.l, 3)
+		x, err := s.pop()
+		if assert.NoError(t, err) {
+			assert.Equal(t, 4, x)
+		}
+		x, err = s.pop()
+		if assert.NoError(t, err) {
+			assert.Equal(t, 3, x)
+		}
+		x, err = s.pop()
+		if assert.NoError(t, err) {
+			assert.Equal(t, 2, x)
+		}
+		_, err = s.pop()
+		assert.ErrorIs(t, err, errStackEmpty)
+	})
+}
